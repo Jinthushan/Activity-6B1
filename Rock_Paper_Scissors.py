41ddf12e1cfbacfc431f88ea24172cb680522760
Jinthushan <jinthushan.sutharman@ontariotechu.net>
2025-02-13 23:51:12 -0500

    Added a function that generates rock paper or scissors.

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -1,1 +1,11 @@
+import random
+
+def rockpaper():
+    x = random.randint(1,3)
+    if(x == 1):
+        play = "rock"
+    elif(x == 2):
+        play = "paper"
+    elif(x == 3):
+        print('scissors')
 
