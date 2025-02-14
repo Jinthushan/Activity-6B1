9de23fba21d40641155dbe9d56601ea8ca57a916
Jinthushan <jinthushan.sutharman@ontariotechu.net>
2025-02-14 00:11:08 -0500

    Fixed bug and finished up small project.

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -15,18 +15,34 @@ def game():
     while(run == True):
         print("Welcome to Rock Paper Scissors!")
         gameplay,x = rockpaper()
-        player_choice = input("Pick a number from 1 to 3 or press 0 to end the game")
-        player_choice = input("1 is rock, 2 is paper and 3 is scissors!")
+        print("1 is rock, 2 is paper and 3 is scissors!")
+        player_choice = int(input("Pick a number from 1 to 3 or press 0 to end the game: "))
         print("---------------------------------------")
         if(player_choice == 0):
             print("Goodbye!")
             run = False
         else:
             print("I picked " + gameplay)
-            if(player_choice == x):
+            if(gameplay == 'rock' and player_choice == 2):
                 print("You won!")
-            else:
+            elif(gameplay == 'rock' and player_choice == 3):
                 print("You lose")
+            elif(gameplay == 'rock' and player_choice == 1):
+                print("It was a draw")
+
+            if(gameplay == 'paper' and player_choice == 3):
+                print("You won!")
+            elif(gameplay == 'paper' and player_choice == 1):
+                print("You lose")
+            elif(gameplay == 'paper' and player_choice == 2):
+                print("It was a draw")
+
+            if(gameplay == 'scissors' and player_choice == 1):
+                print("You won!")
+            elif(gameplay == 'scissors' and player_choice == 2):
+                print("You lose")
+            elif(gameplay == 'scisssors' and player_choice == 3):
+                print("It was a draw")
 
 game()
 
