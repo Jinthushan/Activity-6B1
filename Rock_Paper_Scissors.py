226b0a4ffcb984ab1dcd04e0c33b03a02e7827eb
Jinthushan <jinthushan.sutharman@ontariotechu.net>
2025-02-13 23:59:06 -0500

    Made rock paper scissors.

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -7,5 +7,27 @@ def rockpaper():
     elif(x == 2):
         play = "paper"
     elif(x == 3):
-        print('scissors')
+        play = 'scissors'
+    return play,x
+
+def game():
+    run = True
+    while(run == True):
+        print("Welcome to Rock Paper Scissors!")
+        gameplay,x = rockpaper()
+        player_choice = input("Pick a number from 1 to 3 or press 0 to end the game")
+        player_choice = input("1 is rock, 2 is paper and 3 is scissors!")
+        print("---------------------------------------")
+        if(player_choice == 0):
+            print("Goodbye!")
+            run = False
+        else:
+            print("I picked " + gameplay)
+            if(player_choice == x):
+                print("You won!")
+            else:
+                print("You lose")
+
+game()
+
 
